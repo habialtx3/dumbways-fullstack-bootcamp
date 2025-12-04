5965a09da8cad522979d7df1f5f1bb03b75dfe97
MREZA <rezaworku31@gmail.com>
2025-12-04 16:18:08 +0700

    updated : polishing ui and adding id to each button

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const form = document.getElementById('projectForm')
 const fileInput = document.getElementById('file_input')
-
+let id = 0;
 form.addEventListener('submit',function(e){
     e.preventDefault()
 
-    let id = 0;
+    
     const project_name = document.getElementById('project_name').value;
     const start_date = document.getElementById('start_date').value;
     const end_date = document.getElementById('end_date').value;
@@ -29,7 +29,7 @@ form.addEventListener('submit',function(e){
     containerCard.classList.add('col-md-4','mb-4');
 
     const card = document.createElement('div')
-    card.classList.add('card')
+    card.classList.add('card','h-')
 
     let dummyUrl = "https://via.placeholder.com/300x200?text=No+Image"
     if(file){
@@ -37,21 +37,21 @@ form.addEventListener('submit',function(e){
     }
 
     const img = document.createElement('img')
-    img.src(dummyUrl)
+    img.src = dummyUrl
     img.classList.add('card-img-top')
 
     const cardBody = document.createElement('div')
     cardBody.classList.add('card-body')
 
-    cardBody.innerHTML(
+    cardBody.innerHTML =
         `
         <h5 class="card-title">${project_name}</h5>
-        <h5 class="card-text">${description}</h5>
+        <p class="card-text">${description}</p>
         <h5 class="text-text-muted">from ${start_date} to ${end_date}</h5>
         <h5 class="fw-bold">Technologies : ${technologies}</h5>
         <a href="item/${id}" class="btn btn-dark">Details</a>
         `
-    )
+    
 
     card.appendChild(img)
     card.appendChild(cardBody)
@@ -59,4 +59,5 @@ form.addEventListener('submit',function(e){
     cardList.appendChild(containerCard)
 
     form.reset()
+    id++;
 })
