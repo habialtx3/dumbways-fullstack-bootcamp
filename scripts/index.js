db2cdfa29c00d752f6ff762a01c44487989b57a1
MREZA <rezaworku31@gmail.com>
2025-12-08 17:03:44 +0700

    created : edit function project

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,15 @@
 const form = document.getElementById('projectForm')
 const fileInput = document.getElementById('file_input')
+let idEdit = null
+
 
 let projects = JSON.parse(localStorage.getItem("projects")) || [];
 createCard(projects);
+
+const project_name = document.getElementById('project_name');
+const start_date = document.getElementById('start_date');
+const end_date = document.getElementById('end_date');
+const description = document.getElementById('description');
 
 console.log(projects)
 
@@ -16,8 +23,26 @@ function deleteProject(id, event) {
     alert('Item telah dihapus')
 }
 
-function editProject(id) {
-    console.log(`mengedit ${id}`)
+function editProject(id, event) {
+    event.stopPropagation()
+
+    const editedProject = projects.find(project => project.id === id)
+    if (!editedProject) {
+        return alert('project tidak ada')
+    }
+
+    idEdit = id
+
+    project_name.value = editedProject.name
+    start_date.value = editedProject.start_date
+    end_date.value = editedProject.end_date
+    description.value = editedProject.description
+
+    document.querySelectorAll("input[type=checkbox]").forEach(checked_item => {
+        checked_item.checked = editedProject.technologies.includes(checked_item.value)
+    })
+
+    document.getElementById('submitBtn').innerText = "Update Project";
 }
 
 function fileBase64(file) {
@@ -51,7 +76,7 @@ function createCard(projects) {
             <p class="text-muted">${project.start_date} to ${project.end_date} </p>
             <p class="fw-bold">${project.technologies}</p>
             <div class="d-flex gap-3 mb-3 align-content-center justify-content-center">
-            <a class="btn btn-dark" onclick="editProject('${project.id}')">Edit</a>
+            <a class="btn btn-dark" onclick="editProject('${project.id}',event)">Edit</a>
                         <a class="btn btn-dark" onclick="deleteProject('${project.id}',event)">Delete</a>
             </div>
             </div>
@@ -72,13 +97,7 @@ function goToDetailPage(id) {
 
 form.addEventListener('submit', async function (e) {
     e.preventDefault()
-
-    const project_name = document.getElementById('project_name').value;
-    const start_date = document.getElementById('start_date').value;
-    const end_date = document.getElementById('end_date').value;
-    const description = document.getElementById('description').value;
     const file = fileInput.files[0]
-
     const technologies = [];
     document.querySelectorAll("input[type='checkbox']:checked").forEach(
         item => {
@@ -86,15 +105,40 @@ form.addEventListener('submit', async function (e) {
         })
 
     let fileBase64String = null;
-    if(file){
+    if (file) {
         fileBase64String = await fileBase64(file)
     }
+
+    if (idEdit) {
+        const id = projects.findIndex(p => p.id === idEdit)
+
+        projects[id].name = project_name.value
+        projects[id].description = description.value
+        projects[id].start_date = start_date.value
+        projects[id].end_date = end_date.value
+        projects[id].technologies = technologies
+
+        if (fileBase64String) {
+            projects[id].file = fileBase64String
+        }
+
+        localStorage.setItem('projects', JSON.stringify(projects))
+        idEdit = null
+        document.getElementById('submitBtn').innerText = 'Submit'
+
+        alert('Proyek berhasil diperbarui')
+        form.reset()
+
+        createCard(projects)
+        return
+    }
+
     const project = {
         id: `Proyek-${Date.now()}`,
-        name: project_name,
-        start_date: start_date,
-        end_date: end_date,
-        description: description,
+        name: project_name.value,
+        start_date: start_date.value,
+        end_date: end_date.value,
+        description: description.value,
         technologies: technologies,
         file: fileBase64String
     }
