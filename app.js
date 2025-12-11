741c6bf3d6374f085e10cb4dbb8720d271f2100b
MREZA <rezaworku31@gmail.com>
2025-12-11 17:11:01 +0700

    updated : cleaning the code

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 const express = require("express");
 const path = require("path");
-const ejs = require("ejs"); // <-- WAJIB
+const ejs = require("ejs"); 
 
 const app = express();
 const port = 3000;
 
-// View engine HTML dengan EJS render
 app.engine('ejs', ejs.renderFile);
 app.set('view engine', 'ejs'); 
 app.set('views', path.join(__dirname, 'views'));
 
-// Public folder
-
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Import routes
 const webRoutes = require("./routes/web");
 app.use("/", webRoutes);
 
