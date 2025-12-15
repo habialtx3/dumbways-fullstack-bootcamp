32b3a4bd35537d8ca9609b74ca32751bf355d34f
MREZA <rezaworku31@gmail.com>
2025-12-15 19:10:11 +0700

    update : changing image into default image

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const path = require("path");
-const ejs = require("ejs"); 
+const ejs = require("ejs");
 const db = require('./config/db.js')
 
 const app = express();
 const port = 3000;
 
 app.engine('ejs', ejs.renderFile);
-app.set('view engine', 'ejs'); 
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 const webRoutes = require("./routes/web");
 app.use("/", webRoutes);
+
 
 app.listen(port, () => {
   console.log("Server running at http://localhost:" + port);
--- routes/web.js
+++ routes/web.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const db = require('../config/db.js')
+const db = require('../config/db.js');
+const pool = require("../config/db.js");
 
 router.get("/", (req, res) => {
     res.render("index");
@@ -8,13 +9,40 @@ router.get("/", (req, res) => {
 });
 
 router.get("/project", async (req, res) => {
-    const result = await db.query("SELECT * FROM projects ORDER BY id");
-    res.render("project", { 
+    const result = await db.query("SELECT * FROM projects ORDER BY id DESC");
+    res.render("project", {
         title: "My Project Page",
-        projects : result.rows 
+        projects: result.rows
     });
 
 });
+
+router.post("/project", async (req, res) => {
+    const {
+        name,
+        start_date,
+        end_date,
+        description,
+        technologies,
+        image
+    } = req.body
+
+    const techString = Array.isArray(technologies) ? technologies.join(", "): technologies ||"";
+
+    await pool.query(
+        `INSERT INTO projects
+        (name, start_date,end_date,description,technologies,image)
+        VALUES ($1,$2,$3,$4,$5,$6)`, [
+        name,
+        start_date,
+        end_date,
+        description,
+        techString,
+        image || null
+    ]
+    )
+    res.redirect("/project")
+})
 
 router.get("/contact", (req, res) => {
     res.render("contact");
