8160a157a27e32200d37c574605b620e66309b99
MREZA <rezaworku31@gmail.com>
2025-12-16 18:32:20 +0700

    created: login/register with session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const ejs = require("ejs");
 const db = require('./config/db.js')
+const session = require('express-session')
 
 const app = express();
 const port = 3000;
@@ -15,9 +16,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+app.use(session({
+    secret: "secret-key-project",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60
+    }
+}));
+
+app.use((req, res, next) => {
+    res.locals.user = req.session.user || null;
+    next();
+});
+
 const webRoutes = require("./routes/web");
 app.use("/", webRoutes);
-
 
 app.listen(port, () => {
   console.log("Server running at http://localhost:" + port);
--- routes/web.js
+++ routes/web.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const db = require('../config/db.js');
 const pool = require("../config/db.js");
+const bcrypt = require('bcrypt')
 
 router.get("/", (req, res) => {
     res.render("index");
@@ -27,7 +28,7 @@ router.post("/project", async (req, res) => {
         image
     } = req.body
 
-    const techString = Array.isArray(technologies) ? technologies.join(", "): technologies ||"";
+    const techString = Array.isArray(technologies) ? technologies.join(", ") : technologies || "";
 
     await pool.query(
         `INSERT INTO projects
@@ -47,5 +48,87 @@ router.post("/project", async (req, res) => {
 router.get("/contact", (req, res) => {
     res.render("contact");
 });
+
+router.get("/login", (req, res) => {
+    res.render("login");
+});
+
+router.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        
+        if (!email || !password) {
+            return res.status(400).send("Email dan password wajib diisi");
+        }
+
+        const result = await pool.query(
+            "SELECT * FROM users WHERE email = $1",
+            [email]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(401).send("Email atau password salah");
+        }
+
+        const user = result.rows[0];
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(401).send("Email atau password salah");
+        }
+
+        req.session.user = {
+            id: user.id,
+            username: user.username,
+            email: user.email
+        };
+
+        res.redirect("/");
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Server error");
+    }
+});
+
+
+router.get("/register", (req, res) => {
+    res.render("register");
+});
+
+router.post("/register", async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        if (!username || !email || !password) {
+            return res.status(400).send("Data tidak lengkap");
+        }
+        const checkUser = await pool.query(
+            "SELECT * FROM users WHERE email = $1 OR username = $2",
+            [email, username]
+        );
+        if (checkUser.rows.length > 0) {
+            return res.status(400).send("Email atau username sudah terdaftar");
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await pool.query(
+            `INSERT INTO users (username, email, password)
+             VALUES ($1, $2, $3)`,
+            [username, email, hashedPassword]
+        );
+        res.redirect("/login");
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Server error");
+    }
+});
+
+router.get("/logout", async (req, res) => {
+    req.session.destroy(err => {
+        if (err) {
+            return res.send('Gagal Logout');
+        }
+        res.redirect("/login");
+    })
+})
 
 module.exports = router;
